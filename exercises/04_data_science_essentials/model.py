1d0d2d10c4f2e678fe153964ad52c88dd49b89b2
SmokeShine <prateekgupta.nitjsr@gmail.com>
2019-04-27 16:30:55 +0530

    Completed PyCon Assignments

--- exercises/04_data_science_essentials/model.py
+++ exercises/04_data_science_essentials/model.py
@@ -9,20 +9,34 @@ from sklearn.metrics import classification_report
 
 def prediction():
     # Exercise A: Load the data
-    
-
+    df=pd.read_csv('data/talks.csv')
+   
     # Exercise B: Feature Extraction
-
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2), stop_words="english")
+    year_labeled=2017
+    year_predict=2018
+    vectorized_text_labeled = vectorizer.fit_transform(df[df.year==year_labeled]['description'])
+    vectorized_text_predict = vectorizer.transform(df[df.year==year_predict]['description'])
 
     # Exercise C: Split into Training and Testing Set
-
+    labels = df[df.year == 2017]['label']
+    test_size=0.3
+    X_train, X_test, y_train, y_test = train_test_split(vectorized_text_labeled, labels, test_size=test_size, random_state=1)
 
     # Exercise D: Train the model
-
+    classifier = LinearSVC()
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    report = sklearn.metrics.classification_report(y_test, y_pred)
 
     # Exercise E: Make Predictions
+    predicted_talks_vector = classifier.predict(vectorized_text_predict)
+    df_2018 = df[df.year==2018]
 
-
+    # Offset the rows by 2017 talks
+    predicted_talk_indexes = predicted_talks_vector.nonzero()[0] + len(df[df.year==2017])
+    # your solution goes here
     # change the empty data frame with your predictions data frame
-    results = pd.DataFrame({'foo':[]})
-    return results.to_dict(orient='records')
+    results = df_2018.loc[predicted_talk_indexes][['id','description','presenters','title','location','talk_dt']]
+
+    return results.to_json(orient='records')
